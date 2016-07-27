ae6fd21a64db55f1ebabdfa9172cdfdac2cc471a
Seulki-Kim <Seulki-Kim@LG-X4>
2016-07-27 18:22:31 +0900

    --- automatic rebase to remote; old local SHA: 8202393901dae96bcfe32a9360abfbcd20244916 ---

--- src/application/Kefico.java
+++ src/application/Kefico.java
@@ -275,7 +275,7 @@ public class Kefico extends RoboticsAPIApplication {
 		switch (type) {
 		case Electric:
 			Frame approach = pick_aprGrip.copyWithRedundancy();
-			approach.transform(Transformation.ofTranslation(0, 0, -100));
+			approach.transform(Transformation.ofTranslation(0, 0, -50));
 			
 			tcp.move(lin(place_aprGrip).setCartVelocity(500));
 			tcp.moveAsync(lin(approach).setCartVelocity(500).setBlendingRel(0.2));
