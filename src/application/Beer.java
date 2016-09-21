35f1b6059e1c1b00e1bd0ca44855eec0036675a2
Seulki-Kim <Seulki-Kim@LG-X4>
2016-09-21 10:57:30 +0900

    --- automatic rebase to remote; old local SHA: 426542697b64786ebd223c3bb891d514373b3fb1 ---

--- src/application/Beer.java
+++ src/application/Beer.java
@@ -218,12 +218,15 @@ public class Beer extends RoboticsAPIApplication {
 			target.transform(glassBase, Transformation.ofTranslation(0, 0, detectedZOffset+37+40));
 			Frame targetGripApr = target.copyWithRedundancy();
 			targetGripApr.transform(glassBase, Transformation.ofDeg(-30, 0, 0, 0, -20, 0));
-			Frame targetAir = target.copyWithRedundancy();
-			targetAir.transform(glassBase, Transformation.ofDeg(-30, 0, -70, 0, -20, 0));
+			Frame targetAir02 = target.copyWithRedundancy();
+			targetAir02.transform(glassBase, Transformation.ofDeg(-30, 0, -70, 0, -20, 0));
+			Frame targetAir01 = target.copyWithRedundancy();
+			targetAir01.transform(glassBase, Transformation.ofDeg(0, 0, -70, 0, -20, 0));
 			
 			tcpTip.move(ptp(lbr.getCurrentCartesianPosition(tcpTip)));
 			tcpTip.moveAsync(linRel(0, 0, -50).setCartVelocity(1000).setBlendingRel(0.1));
-			tcpGrip.moveAsync(lin(targetAir).setJointVelocityRel(1.0).setBlendingRel(0.1));
+			tcpGrip.moveAsync(lin(targetAir01).setJointVelocityRel(1.0).setBlendingRel(0.1));
+			tcpGrip.moveAsync(lin(targetAir02).setJointVelocityRel(1.0).setBlendingRel(0.1));
 			tcpGrip.moveAsync(lin(targetGripApr).setCartVelocity(600).setBlendingRel(0.1));
 			tcpGrip.move(lin(target).setCartVelocity(300));
 			
